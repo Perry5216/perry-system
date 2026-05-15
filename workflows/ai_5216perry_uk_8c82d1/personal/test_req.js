01aa0a8af4bb775b55289047c3917a85acc0a7fb
Anthony Perry <40871197+Perry5216@users.noreply.github.com>
2026-05-15 18:24:46 +0100

    Remove old n8n workflows tree
    The n8n-based workflow files predate Perry and are superseded by the
    @perry/projects engine + state-store. Removing the tree to reduce
    confusion about which pipeline is authoritative.

--- workflows/ai_5216perry_uk_8c82d1/personal/test_req.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const wf = require('./2 - Dossier to Full Outline.workflow.ts');
-console.log(wf);
--- workflows/ai_5216perry_uk_anthony_p/personal/fix_bt.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('d:\\\\n8n\\\\workflows\\\\ai_5216perry_uk_anthony_p\\\\personal\\\\1 - Braindump to Dossier.workflow.ts', 'utf8');
-
-// The issue is ```json inside template literals. We can just change it to 'json' to be absolutely safe across all quotes.
-code = code.replace(/```json/g, "'json'").replace(/```/g, "'''");
-
-fs.writeFileSync('d:\\\\n8n\\\\workflows\\\\ai_5216perry_uk_anthony_p\\\\personal\\\\1 - Braindump to Dossier.workflow.ts', code, 'utf8');
-console.log('Fixed backticks!');
